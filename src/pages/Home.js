35c8ffd3bdf89ba46a336fd7920268c0771c3804
Nastasiia03 <abernatskao@gmail.com>
2023-03-14 22:59:30 +0200

    amended

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,28 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
 export const Home = () => {
+    const [films, setFilms] = useState([]);
+
+    useEffect(() => {
+      async function fetchData() {
+      try {
+          const url = 'https://api.themoviedb.org/3/trending/movie/day?';
+          const key = '6bf6ef2ab5ece9f25b4bdae1dc149130';
+          const response = await axios.get(`${url}api_key=${key}`);
+        setFilms([...response.data.results]);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchData();
+    
+    }, [])
+    
+
     return <div>
-        <h2>Home page</h2>
+        <h2>Trending today</h2>
+        <ul>{films.map(film => (<li key={film.id}><p>{ film.original_title}</p></li>))}</ul>
     </div>
 }
--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,10 +1,52 @@
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
 
 export const Movies = () => {
+  const [cards, setCards] = useState([]);
+  // const [page, setPage] = useState(1);
+  const [textSearch, setTextSearch] = useState("");
+
+  // const [error, setError] = useState("");
+
+
+  useEffect(() => {
+    if (textSearch !== "") {
+      async function fetchFilms(textSearch) {
+        try {
+          const url = 'https://api.themoviedb.org/3/search/movie?';
+          const key = '6bf6ef2ab5ece9f25b4bdae1dc149130';
+          const response = await axios.get(`${url}api_key=${key}&query=${textSearch}`);
+          setCards([...response.data.results]);
+          console.log(response.data.results)
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+      fetchFilms();
+    }
+  }, [textSearch])
+
+  const handleChange = (event) => {
+    setTextSearch(event.target.value) 
+    
+  }
+
+  const handleSubmit = (e, textSearch) => {
+    e.preventDefault()
+    if (!textSearch) {
+      return toast.error('Sorry, the search field is empty 😒')
+    }
+    setTextSearch(textSearch);
+    setCards([]);
+  }
+  
     return <div>
-      <form>
-        <input type="text"></input>
+      <form onSubmit={handleSubmit}>
+        <input type="text" autoComplete="off" value={textSearch} onChange={handleChange}></input>
         <button type="submit"></button>
-        </form>
+        <ul>{cards.map(card => (<li key={card.id}><p>{ card.original_title}</p></li>))}</ul>
+      </form>
+      <Toaster position="top-right"/>
     </div>
 }
